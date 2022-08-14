dd07961190dd42fb2103b7a5dc741131ffc11c52
Alberto GAray <yarag44@hotmail.com>
2022-08-14 12:41:13 -0700

    Change Port Execution

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app= express();
 
-const port = process.env.PORT | 3000;
+//const port = process.env.PORT | 3000;
 
 
 app.use(function(req, res, next) {
@@ -93,7 +93,7 @@ app.get("/report",async (req,res) => {
 });
 
 
-app.listen (port, () => {
+app.listen ((process.env.PORT || 5000), () => {
 
     console.log("Server Running on port 3000");
 
